825d53e4857f1ba071f5cb5ae0f53cf7455525f5
Akshay Rathod <kr.akshay234@gmail.com>
2023-07-08 14:28:01 +0530

    updated

--- controllers/main.js
+++ controllers/main.js
@@ -19,7 +19,7 @@ const dashboard = async (req, res) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new CustomAPIError("No token provided", 401);
-  } 
+  }
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
